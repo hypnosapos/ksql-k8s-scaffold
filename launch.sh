97a10310b76cd815236f9ecf51d8fedb1313425a
laetitiae <leti.garcia.martin@gmail.com>
2018-08-23 13:12:37 +0200

    Add target for cli installation

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-make gke-bastion gke-create-cluster gke-ui-login-skip gke-tiller-helm gke-proxy gke-ui gke-ksql-install
+make gke-bastion gke-create-cluster gke-ui-login-skip gke-tiller-helm gke-proxy gke-ui gke-ksql-install ksql-gke-conf
